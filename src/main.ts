7931bdae291dbb78d81b8d58bcc94e7b27773457
Julian Gassama <gassamajulian@gmail.com>
2025-02-03 18:27:54 +0100

    v2

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { destroyPlatform, getPlatform } from '@angular/core';
 
-// Verificar si existe una plataforma Angular y destruirla si es necesario
 if (getPlatform()) {
   destroyPlatform();
 }
 
-// Inicializar la aplicación Angular
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient()],
 }).then(ref => {
-  // Almacenar la referencia de la plataforma en window.ngRef
+
   (window as any).ngRef = ref;
 }).catch(err => console.error(err));
